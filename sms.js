e52675b7e6d9619e83bb543e07fce3f8d23a8b48
Thuc Katy <katyperrycbt@gmail.com>
2020-11-19 12:17:28 +0700

    Delete comments

--- sms.js
+++ sms.js
@@ -52,8 +52,5 @@ app.use('/school', authMiddleware.default, authMiddleware.requireAuth, csrfProte
 
 app.listen(port, function () {
   console.log('Server at port '  + port + ' is running...!');
-  //cre
-  //test
-  //opp[s
   open('http://localhost:6969', {app: edge});
 });
