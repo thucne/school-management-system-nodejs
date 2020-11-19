7a7540069635d791872b20917c34979aa46fb63d
Thuc Katy <katyperrycbt@gmail.com>
2020-11-19 12:36:54 +0700

    Update Credential

--- sms.js
+++ sms.js
@@ -52,5 +52,6 @@ app.use('/school', authMiddleware.default, authMiddleware.requireAuth, csrfProte
 
 app.listen(port, function () {
   console.log('Server at port '  + port + ' is running...!');
+  console.log('Ji');
   open('http://localhost:6969', {app: edge});
 });
