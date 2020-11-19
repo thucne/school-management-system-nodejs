6095415c4d315d15889d8d8e8030c2c4ba7e5d49
Thuc Katy <katyperrycbt@gmail.com>
2020-11-19 12:06:28 +0700

    Update Test

--- sms.js
+++ sms.js
@@ -53,5 +53,6 @@ app.use('/school', authMiddleware.default, authMiddleware.requireAuth, csrfProte
 app.listen(port, function () {
   console.log('Server at port '  + port + ' is running...!');
   //cre
+  //test
   open('http://localhost:6969', {app: edge});
 });
